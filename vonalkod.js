a83d69f721fb2d5094437e135e67ae6680503f70
pelbart <fbpelbi@hotmail.com>
2024-03-13 13:54:52 +0100

    modify

--- vonalkod.js
+++ vonalkod.js
@@ -2,24 +2,51 @@ let selectedDeviceId;
 const codeReader = new ZXing.BrowserMultiFormatReader()
 console.log('ZXing code reader initialized')
 
-/*const codeReader = new ZXing.BrowserQRCodeReader();
-let videoDevice = false;
+codeReader.listVideoInputDevices()
+    .then((videoInputDevices) => {
+        const sourceSelect = document.getElementById('sourceSelect')
+        selectedDeviceId = videoInputDevices[0].deviceId
+        if (videoInputDevices.length >= 1) {
+            videoInputDevices.forEach((element) => {
+                const sourceOption = document.createElement('option')
+                sourceOption.text = element.label
+                sourceOption.value = element.deviceId
+                sourceSelect.appendChild(sourceOption)
+            })
 
-codeReader.getVideoInputDevices().then((videoInputDevices) => {
-    console.log('codeReader');
-    if (videoInputDevices.length >= 1) {
-        console.log('videoInputDevices.length >= 1');
-        videoDevice = true;
-        //startScanner();
-        document.getElementById("videoWrapper").style.display = "block";
-        document.getElementById("videoInputDevices").innerHTML = videoInputDevices.length;
-    } else {
-        console.log('videoInputDevices.length: ' + videoInputDevices.length);
-        document.getElementById("videoInputDevices").innerHTML = videoInputDevices.length;
-    }
-}).catch((err) => {
-    console.error(err)
-});*/
+            sourceSelect.onchange = () => {
+                selectedDeviceId = sourceSelect.value;
+            };
+
+            const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+            sourceSelectPanel.style.display = 'block'
+        }
+
+        /*document.getElementById('startButton').addEventListener('click', () => {
+            codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+                if (result) {
+                    console.log(result)
+                    document.getElementById('result').textContent = result.text
+                }
+                if (err && !(err instanceof ZXing.NotFoundException)) {
+                    console.error(err)
+                    document.getElementById('result').textContent = err
+                }
+            })
+            console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+        })
+
+        document.getElementById('resetButton').addEventListener('click', () => {
+            codeReader.reset()
+            document.getElementById('result').textContent = '';
+            console.log('Reset.')
+        })*/
+
+    })
+    .catch((err) => {
+        console.error(err)
+        document.getElementById("result").innerHTML = err;
+    });
 
 
 function startScan() {
